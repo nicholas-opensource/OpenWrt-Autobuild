617415b44f45d97f5ca91f935ccd42c5800529d7
Nicholas Sun <nicholas-sun@outlook.com>
2022-12-05 23:27:36 +0800

    x86: Add model name Switch Cflags from O3 to O2 for architecture x86
    Signed-off-by: Nicholas Sun <nicholas-sun@outlook.com>

--- SCRIPTS/X86/02_X86.sh
+++ SCRIPTS/X86/02_X86.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 clear
 
+# GCC CFlags for x86
+sed -i 's/O3 -Wl,--gc-sections/O2 -Wl,--gc-sections -mtune=goldmont-plus/g' include/target.mk
+
 # Addition-Trans-zh-master and fix APNS
 cp -rf ../PATCH/duplicate/addition-trans-zh-x86 ./package/utils/addition-trans-zh
 cp -rf ../PATCH/duplicate/files ./files
+
+# Echo Model
+echo '# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+grep "Default string" /tmp/sysinfo/model >> /dev/null
+if [ $? -ne 0 ];then
+    echo should be fine
+else
+    echo "Compatible PC" > /tmp/sysinfo/model
+fi
+exit 0
+'> ./package/base-files/files/etc/rc.local
 
 # Match Vermagic
 latest_release="$(curl -s https://api.github.com/repos/openwrt/openwrt/tags | grep -Eo "22.03.+[0-9\.]" | head -n 1)"
