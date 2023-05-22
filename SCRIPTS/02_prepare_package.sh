eedcaaddac2342d1b27b2600e37325aa1457ecb7
Nicholas Sun <nicholas-sun@outlook.com>
2023-05-23 01:21:04 +0800

    Fix build
    Signed-off-by: Nicholas Sun <nicholas-sun@outlook.com>

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -62,6 +62,8 @@ rm -rf ./target/linux/rockchip/patches-5.10/002-net-usb-r8152-add-LED-configurat
 rm -rf ./target/linux/rockchip/patches-5.10/003-dt-bindings-net-add-RTL8152-binding-documentation.patch
 rm -rf ./package/firmware/linux-firmware/intel.mk
 cp -rf ../lede/package/firmware/linux-firmware/intel.mk ./package/firmware/linux-firmware/intel.mk
+rm -rf ./package/firmware/linux-firmware/mediatek.mk
+cp -rf ../lede/package/firmware/linux-firmware/mediatek.mk ./package/firmware/linux-firmware/mediatek.mk
 rm -rf ./package/firmware/linux-firmware/Makefile
 cp -rf ../lede/package/firmware/linux-firmware/Makefile ./package/firmware/linux-firmware/Makefile
 mkdir -p target/linux/rockchip/files-5.10
