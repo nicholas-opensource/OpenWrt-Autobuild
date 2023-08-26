3281a06fb9154962138f38b3a1374108e6bf1764
Nicholas Sun <nicholas-sun@outlook.com>
2023-08-26 22:09:26 +0800

    Add checksum in zip
    Signed-off-by: Nicholas Sun <nicholas-sun@outlook.com>

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -4,6 +4,9 @@ rm -rf `ls | grep -v "squashfs"`
 ls *rootfs.img.gz | xargs rm -fr
 gzip -d *.gz
 gzip *.img
+for gzfirm in *.gz; do
+  sha256sum "$gzfirm" >> sha256sum.txt
+done
 mv ../../../../.config config-full
 
 exit 0
